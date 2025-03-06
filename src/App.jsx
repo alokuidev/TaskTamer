9786b610d2a2feb830c1c4560baecce20b1088c2
warrioralok <34855411+warrioralok@users.noreply.github.com>
2025-03-07 00:30:52 +0530

    styling and html updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <div className="todo-container">
+        <h1>Todo List</h1>
+        
+        <div className="todo-input">
+            <input type="text" id="todo-text" placeholder="Enter a task..."/>
+            <button id="add-btn">Add Task</button>
+        </div>
+
+        <ul id="todo-list"></ul>
+
+        <button id="reset-btn" className="reset">Reset All</button>
+    </div>
+
     </>
   )
 }
