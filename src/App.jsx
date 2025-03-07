5de17cfc034eee0d94ab456377d72777ed38aa1c
warrioralok <34855411+warrioralok@users.noreply.github.com>
2025-03-08 00:40:42 +0530

    html css base ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-
-
 function App() {
-
   return (
     <>
-       <div className="todo-container">
+      <div className="todo-container">
         <h1>Todo List</h1>
-        
+
         <div className="todo-input">
-            <input type="text" id="todo-text" placeholder="Enter a task..."/>
-            <button id="add-btn">Add Task</button>
+          <input type="text" id="todo-text" placeholder="Enter a task..." />
+          <button id="add-btn">Add Task</button>
         </div>
 
-        <ul id="todo-list"></ul>
+        <ul id="todo-list">
+          <li className="todo-item">
+            <input type="checkbox" class="toggle-complete" />
+            <span className="task-text">Task Name</span>
+            <button className="delete-btn">❌</button>
+          </li>
+        </ul>
 
-        <button id="reset-btn" className="reset">Reset All</button>
-    </div>
-
+        <button id="reset-btn" className="reset">
+          Reset All
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
